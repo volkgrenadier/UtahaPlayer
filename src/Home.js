c78ca0d9c9d2d0508ba10da139843dac962195df
zhaozili <1596782951@qq.com>
2022-11-27 23:42:56 +0800

    utaha music, some features has been added.

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './home.module.scss'
+const Home = () => {
+  return (
+    <div className={styles.home_container}>Ho55me</div>
+  )
+}
+
+export default Home
---
+++ src/Player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './player.module.scss'
+const Player = () => {
+  return (
+    <div className={styles.player_container}>
+        123
+    </div>
+  )
+}
+
+export default Player
