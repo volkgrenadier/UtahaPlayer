2c56f508947a3e2cc85902f0641d9689462283c6
zhaozili <1596782951@qq.com>
2022-12-03 23:04:27 +0800

    update the struction of program and some features of Audio.js

---
+++ src/component/Dashboard/NavColumn/NavColumn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './navcolumn.module.scss'
+const NavColumn = () => {
+  return (
+    <div className={styles.navcolumn_container}>
+        <div className={styles.navcolumn_listContainer}></div>
+    </div>
+  )
+}
+
+export default NavColumn
