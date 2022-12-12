7172dce9b1dab2279dc1be35f7488e98b7084b93
zhaozili <1596782951@qq.com>
2022-12-12 23:35:47 +0800

    primarily finished the UI of Display.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux'
-import Home from './component/Dashboard/Dashboard';
+import Dashboard from './component/Dashboard/Dashboard';
 import Header from './component/Header/Header';
 import Player from './component/Player/Player';
 import DragWindow from './component/DragWindow/DragWindow';
@@ -28,7 +28,7 @@ function App() {
       <div className={styles.app_login_Container} ref={loginContainer}>
         <Login />
       </div>
-      <Home />
+      <Dashboard />
       <Player />
     </div>
   );
--- src/component/Dashboard/Display/Display.js
+++ src/component/Dashboard/Display/Display.js
@@ -1,8 +1,47 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { getHomeInfo, getRecommandNewSong } from '../../request/request'
+import styles from './display.module.scss'
+import { testData } from './testdata'
 const Display = () => {
+  let data = testData
+  useEffect(() => {
+    // getHomeInfo()
+    // .then(r => {
+    //   console.log(r)
+    // })
+    // getRecommandNewSong()
+    // .then(r => {
+    //   console.log(r)
+    // })
+    data.map(item => {
+      if (item.name.length > 7) {
+        item.name = `${item.name.slice(0,6)}...`
+      }
+      if (item.artists[0].name > 8) {
+        item.artists[0].name = `${item.name.slice(0,7)}...`
+      }
+    })
+  },[])
+  console.log(testData)
   return (
-    <div>Display</div>
+    <div className={styles.display_container}>
+      <div className={styles.display_banner_container}>banner</div>
+      <ul className={styles.display_newSongRec_container}>
+        {
+          data.map(item => {
+            return (
+              <li key={item.id} className={styles.display_newSongRec_song_container}>
+                <img src={require('./utaha_min.png')} alt="456465" className={styles.display_newSongRec_song_img} loading="lazy"/>
+                <div className={styles.display_newSongRec_song_text_Container}>
+                  <p className={styles.display_newSongRec_song_name}>{item.name}</p>
+                  <p className={styles.display_newSongRec_song_artists}>{item.artists[0].name}</p>
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
   )
 }
 
