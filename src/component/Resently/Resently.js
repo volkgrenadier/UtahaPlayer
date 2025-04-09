9f1353371a3407fae1b193390bcf395d1fbedb61
scChess6 <1747617340@qq.com>
2025-04-09 22:06:11 +0800

    add router.md && update component router && add three components

---
+++ src/component/Resently/Resently.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Resently = () => {
+	return (
+		<div>Resently</div>
+	)
+}
+
+export default Resently
--- src/component/Routes/Routes.js
+++ src/component/Routes/Routes.js
@@ -1,13 +1,14 @@
 import { useRoutes } from 'react-router-dom'
 import Home from '../Home/Home'
 const Routes = () => {
-  const routes = useRoutes([
-    {
-      path: '/',
-      element: <Home />
-    },
-  ])
-  return routes
+	const routes = useRoutes([
+		{ path: '/', element: <Home /> },
+		{ path: '/musicplayer', element: <MusicPlayer /> },
+		{ path: '/vedio', element: <Vedio /> },
+		{ path: '/collect', element: <Collect /> },
+		{ path: '/resently', element: <Resently /> }
+	])
+	return routes
 }
 
 export default Routes
