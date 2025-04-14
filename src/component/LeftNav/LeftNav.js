6b178f67dabbd71678d265235bd254b73dc74da2
scChess6 <1747617340@qq.com>
2025-04-14 21:40:45 +0800

    update LeftNav.js&scss

--- src/component/LeftNav/LeftNav.js
+++ src/component/LeftNav/LeftNav.js
@@ -4,24 +4,33 @@ import './LeftNav.scss'
 
 const LeftNav = () => {
 	return (
-		<div className='SideBar'>
-			<nav>
-				<NavLink to="/" className={({ isActive }) => isActive ? "active-link" : ""}>
-					Home
-				</NavLink>
-				<NavLink to="/music" className={({ isActive }) => isActive ? "active-link" : ""}>
-					音乐
-				</NavLink>
-				<NavLink to="/vedio" className={({ isActive }) => isActive ? "active-link" : ""}>
-					视频
-				</NavLink>
-				<NavLink to="/collect" className={({ isActive }) => isActive ? "active-link" : ""}>
-					收藏
-				</NavLink>
-				<NavLink to="/resently" className={({ isActive }) => isActive ? "active-link" : ""}>
-					最近播放
-				</NavLink>
-			</nav>
+		<div className='sideBar'>
+			<NavLink to="/" className={({ isActive }) =>
+				isActive ? 'nav-item active' : 'nav-item'
+			}>
+				Home
+			</NavLink>
+			<NavLink to="/music" className={({ isActive }) =>
+				isActive ? 'nav-item active' : 'nav-item'
+			}>
+				音乐
+			</NavLink>
+			<NavLink to="/vedio" className={({ isActive }) =>
+				isActive ? 'nav-item active' : 'nav-item'
+			}>
+				视频
+			</NavLink>
+			<NavLink to="/collect" className={({ isActive }) =>
+				isActive ? 'nav-item active' : 'nav-item'
+			}>
+				收藏
+			</NavLink>
+			<NavLink to="/resently" className={({ isActive }) =>
+				isActive ? 'nav-item active' : 'nav-item'
+			}>
+				最近播放
+			</NavLink>
+
 		</div>
 	)
 
