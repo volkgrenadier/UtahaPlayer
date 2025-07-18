e5d9fd6fbd033a20ca28fba4b3909cfb4797582a
霞ヶ丘詩羽先輩のfans <1596782951@qq.com>
2025-07-18 17:25:43 +0800

    feat: 1. 完成对侧边栏的修改； 2. 图片部分增加新的功能按钮。

--- src/component/LeftNav/LeftNav.js
+++ src/component/LeftNav/LeftNav.js
@@ -1,55 +1,67 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import './LeftNav.scss';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';//最近播放
 import FavoriteIcon from '@mui/icons-material/Favorite';//收藏
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import VideocamIcon from '@mui/icons-material/Videocam';
 import PhotoIcon from '@mui/icons-material/Photo';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import './LeftNav.scss';
 
 const LeftNav = () => {
+	const [expandStatus, setExpandStatus] = useState(false)
 	return (
-		
-		<div className='sideBar'>
-			{/* <NavLink to="/" className={({ isActive }) =>
-				isActive ? 'nav-item active' : 'nav-item'
-			}>
-				Home
-			</NavLink> */}
-			<div className='sideTitle'>
-				媒体库
-			</div>
-			<NavLink to="/music" className={({ isActive }) =>
-				isActive ? 'nav-item active' : 'nav-item'
-			}>
-				<MusicNoteIcon className='nav-icon'/>
-				<div className='nav-txt'>音乐</div>
-			</NavLink>
-			<NavLink to="/video" className={({ isActive }) =>
-				isActive ? 'nav-item active' : 'nav-item'
-			}>
-				<VideocamIcon className='nav-icon'/>
-				<div className='nav-txt'>视频</div>
-			</NavLink>
-			<NavLink to="/photo" className={({ isActive }) =>
-				isActive ? 'nav-item active' : 'nav-item'
-			}>
-				<PhotoIcon className='nav-icon'/>
-				<div className='nav-txt'>图片</div>
-			</NavLink>
-			<NavLink to="/collect" className={({ isActive }) =>
-				isActive ? 'nav-item active' : 'nav-item'
-			}>
-				<FavoriteIcon className='nav-icon'/>
-				<div className='nav-txt'>收藏</div>
-			</NavLink>
-			<NavLink to="/resently" className={({ isActive }) =>
-				isActive ? 'nav-item active' : 'nav-item'
-			}>
-				<AccessTimeIcon className='nav-icon'/>
-				<div className='nav-txt'>最近播放</div>
-			</NavLink>
+		<div className='sideBar_container'>
+			<div className={`sideBar ${expandStatus? 'sideBar-expand': ''}`}>
+				{/* <NavLink to="/" className={({ isActive }) =>
+					isActive ? 'nav-item active' : 'nav-item'
+				}>
+					Home
+				</NavLink> */}
+				{/* <div className='sideTitle'>
+					媒体
+				</div> */}
+				<div 
+					className='nav-expandButton'
+					onClick={() => setExpandStatus(!expandStatus)}
+				>
+					<MenuIcon className='nav-icon'/>
+				</div>
+				<NavLink to="/music" className={({ isActive }) =>
+					isActive ? 'nav-item active' : 'nav-item'
+				}>
+					<MusicNoteIcon className='nav-icon'/>
+					<div className={`nav-txt ${expandStatus? 'nav-txt-show': ''}`}>音乐</div>
+				</NavLink>
+				<NavLink to="/video" className={({ isActive }) =>
+					isActive ? 'nav-item active' : 'nav-item'
+				}>
+					<VideocamIcon className='nav-icon'/>
+					<div className={`nav-txt ${expandStatus? 'nav-txt-show': ''}`}>视频</div>
+				</NavLink>
+				<NavLink to="/photo" className={({ isActive }) =>
+					isActive ? 'nav-item active' : 'nav-item'
+				}>
+					<PhotoIcon className='nav-icon'/>
+					<div className={`nav-txt ${expandStatus? 'nav-txt-show': ''}`}>图片</div>
+				</NavLink>
+				<NavLink to="/collect" className={({ isActive }) =>
+					isActive ? 'nav-item active' : 'nav-item'
+				}>
+					<FavoriteIcon className='nav-icon'/>
+					<div className={`nav-txt ${expandStatus? 'nav-txt-show': ''}`}>收藏</div>
+				</NavLink>
+				<NavLink to="/resently" className={({ isActive }) =>
+					isActive ? 'nav-item active' : 'nav-item'
+				}>
+					<AccessTimeIcon className='nav-icon'/>
+					<div className={`nav-txt ${expandStatus? 'nav-txt-show': ''}`}>最近播放</div>
+				</NavLink>
 
+			</div>
 		</div>
+		
 	)
 
 }
